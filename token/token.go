3f444dc04e53284bed4d8f4a2559acf0555b8a00
Jibran Kalia <jibran.kalia@gmail.com>
2021-05-29 15:18:54 -0500

    add the rest of the tokens

--- token/token.go
+++ token/token.go
@@ -23,6 +23,9 @@ const (
 
 	LT = "<"
 	GT = ">"
+
+	EQ     = "=="
+	NOT_EQ = "!="
 	// Delimiters
 	COMMA     = ","
 	SEMICOLON = ";"
@@ -33,6 +36,11 @@ const (
 	// Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 var keywords = map[string]TokenType{
