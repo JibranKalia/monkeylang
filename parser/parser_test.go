4d01345b48dba6402d3062340cd5291eacc0131d
Jibran Kalia <jibran.kalia@gmail.com>
2021-05-31 12:46:47 -0500

    parser working

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -11,7 +11,7 @@ func TestLetStatements(t *testing.T) {
 	input := `
 let x = 5;
 let y = 10;
-let foobar = 838383
+let foobar = 838383;
 `
 
 	l := lexer.New(input)
@@ -56,12 +56,13 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 		return false
 	}
 	if letStmt.Name.Value != name {
-		t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value) return false
+		t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
+		return false
 	}
 
 	if letStmt.Name.TokenLiteral() != name {
 		t.Errorf("letStmt.Name.TokenLiteral() not '%s'. got=%s",
-		name, letStmt.Name.TokenLiteral())
+			name, letStmt.Name.TokenLiteral())
 		return false
 	}
 	return true
